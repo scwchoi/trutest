13613c60236d670bb2ad8051adc7ac95dc314e44
Scott Choi <scott@Scotts-MacBook-Pro.local>
2013-11-12 14:54:32 -0800

    Clean up

--- recurly/api/accounts-c.php
+++
@@ -1,6 +0,0 @@
-<?php
-$curl = curl_init();
-curl_setopt ($curl, CURLOPT_URL, "https://ace774b375534ada9baa1109a2adc921@trujemi.recurly.com/v2/accounts");
-$result = curl_exec ($curl);
-curl_close ($curl);
-?>
